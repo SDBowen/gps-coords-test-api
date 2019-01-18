9f9ca5152874705de9996a090ed73ebf5328b220
Steven Bowen <sdbowen.git@gmail.com>
2019-01-17 21:00:03 -0700

    Add api post request

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const querystring = require("querystring");
 const https = require("https");
 
 const public_key = "d37555ccc09141848543ab21e287b560";
@@ -11,14 +12,39 @@ https.get(url, res => {
   });
   res.on("end", () => {
     body = JSON.parse(body);
-    console.log(body);
+
+    trainOne = body.ctatt.route[0].train[0];
+
+    let lat = trainOne.lat;
+    let lon = trainOne.lon;
+
+    sendCoords(lat, lon);
   });
 });
 
-// fetch(url)
-//   .then(function(data) {
-//     console.log(data);
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
+sendCoords = (lat, lon) => {
+  var options = {
+    method: "POST",
+    hostname: "intense-everglades-50142.herokuapp.com",
+    path: `/api/coords/480?lat=${lat}&lon=${lon}`,
+    headers: {
+      "cache-control": "no-cache",
+      "Test-Token": "b31495ed-30c2-4594-ab33-b44efe7ba21f"
+    }
+  };
+
+  var req = https.request(options, function(res) {
+    var chunks = [];
+
+    res.on("data", function(chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function() {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+  });
+
+  req.end();
+};
